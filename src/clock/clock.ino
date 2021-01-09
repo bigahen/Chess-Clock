170936855d303f8625644dbc014a439039569e19
Austin Hendrix <bigahen@hotmail.com>
2021-01-09 16:41:56 -0600

    using extended library to print time and added basic countdown functionality

--- src/clock/clock.ino
+++ src/clock/clock.ino
@@ -9,23 +9,41 @@ SevenSegmentTM1637    display(PIN_CLK, PIN_DIO);
 
 const byte PIN_CLK2 = 4;   // define CLK pin (any digital pin)
 const byte PIN_DIO2 = 5;   // define DIO pin (any digital pin)
-SevenSegmentTM1637    display2(PIN_CLK2, PIN_DIO2);
+SevenSegmentExtended    display2(PIN_CLK2, PIN_DIO2);
+
+int maxMiniute = 99; 
+int maxSecond = 59; 
+
+int clockOneMinute = 0;
+int clockTwoMinute = 99;
+
+int clockOneSecond = 0;
+int clockTwoSecond = 59;
 
 // run setup code
 void setup() {
   Serial.begin(9600);         // initializes the Serial connection @ 9600 baud
   display.begin();            // initializes the display
-  display.setBacklight(100);  // set the brightness to 100 %
+  display.setBacklight(50);  // set the brightness to 100 %
   display.print("INIT");      // display INIT on the display
   delay(1000);                // wait 1000 ms
 
   display2.begin();            // initializes the display
-  display2.setBacklight(100);  // set the brightness to 100 %
-  display2.print("INIT");      // display INIT on the display
+  display2.setBacklight(50);  // set the brightness to 100 %
+  display2.printTime(clockTwoMinute, clockTwoSecond, false); 
 };
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(1000); 
+
+  if (clockTwoSecond == 0) {
+    clockTwoSecond = 59; 
+    clockTwoMinute -=1;
+  } else {
+    clockTwoSecond -=1; 
+  }
+
+   display2.printTime(clockTwoMinute, clockTwoSecond, false);
 
 }
